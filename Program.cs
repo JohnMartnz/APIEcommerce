ed78666b0fd546f06b07722c326afa5185462caf
John Mart <juanmart.nt@gmail.com>
2026-03-09 13:29:58 -0600

    Add ProductsController with dependency injection fro IProductRepository and AutoMapper

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConnectionString));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(cfg =>
 {
     cfg.AddMaps(typeof(Program).Assembly);
