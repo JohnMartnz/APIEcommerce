d4c2151994159ca92d3ce55dce391284a8927354
John Mart <juanmart.nt@gmail.com>
2026-03-09 13:28:24 -0600

    Add ProductProfile to map data

---
+++ Mapping/ProductProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using APIEcommerce.Models;
+using APIEcommerce.Models.Dtos;
+using AutoMapper;
+
+namespace APIEcommerce.Mapping;
+
+public class ProductProfile : Profile
+{
+  public ProductProfile()
+  {
+    CreateMap<Product, ProductDto>()
+    .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+    .ReverseMap();
+    CreateMap<Product, CreateProductDto>().ReverseMap();
+    CreateMap<Product, UpdateProductDto>().ReverseMap();
+  }
+}
