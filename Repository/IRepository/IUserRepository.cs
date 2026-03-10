b2acb3fce1d1b21b68e7424e6eaff1e8d733c2b1
John Mart <juanmart.nt@gmail.com>
2026-03-10 11:00:46 -0600

    Add UserRepository to handle data

---
+++ Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using APIEcommerce.Models;
+using APIEcommerce.Models.Dtos;
+
+namespace APIEcommerce.Repository.IRepository;
+
+public interface IUserRepository
+{
+  ICollection<User> GetUsers();
+  User? GetUser(int id);
+  bool IsUniqueUser(string username);
+  Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto);
+  Task<User> Register(CreateUserDto createUserDto);
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,124 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using APIEcommerce.Data;
+using APIEcommerce.Models;
+using APIEcommerce.Models.Dtos;
+using APIEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace APIEcommerce.Repository;
+
+public class UserRepository : IUserRepository
+{
+  private readonly ApplicationDbContext _db;
+  private readonly string? secretKey;
+
+  public UserRepository(ApplicationDbContext db, IConfiguration configuration)
+  {
+    _db = db;
+    secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
+  }
+
+  public ICollection<User> GetUsers()
+  {
+    return _db.Users.OrderBy(user => user.Username).ToList();
+  }
+
+  public User? GetUser(int id)
+  {
+    return _db.Users.FirstOrDefault(user => user.Id == id);
+  }
+
+  public bool IsUniqueUser(string username)
+  {
+    return !_db.Users.Any(user => user.Username.ToLower().Trim() == username.ToLower().Trim());
+  }
+
+  public async Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto)
+  {
+    if (string.IsNullOrEmpty(userLoginDto.Username))
+    {
+      return new UserLoginResponseDto()
+      {
+        Token = "",
+        User = null,
+        Message = "Username is required"
+      };
+    }
+
+    User? user = await _db.Users.FirstOrDefaultAsync<User>(user => user.Username.ToLower().Trim() == userLoginDto.Username.ToLower().Trim());
+    if (user == null)
+    {
+      return new UserLoginResponseDto()
+      {
+        Token = "",
+        User = null,
+        Message = "Username not found"
+      };
+    }
+
+    if (!BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
+    {
+      return new UserLoginResponseDto()
+      {
+        Token = "",
+        User = null,
+        Message = "Credentials are incorrect"
+      };
+    }
+
+    // JWT
+    JwtSecurityTokenHandler handleToken = new JwtSecurityTokenHandler();
+    if (string.IsNullOrWhiteSpace(secretKey))
+    {
+      throw new InvalidOperationException("SecretKey is not set up");
+    }
+    byte[]? key = Encoding.UTF8.GetBytes(secretKey);
+    SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+    {
+      Subject = new ClaimsIdentity(new[]
+      {
+        new Claim("id", user.Id.ToString()),
+        new Claim("user", user.Username),
+        new Claim(ClaimTypes.Role, user.Role ?? string.Empty),
+      }),
+      Expires = DateTime.UtcNow.AddHours(2),
+      SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+    };
+
+    SecurityToken token = handleToken.CreateToken(tokenDescriptor);
+
+    return new UserLoginResponseDto()
+    {
+      Token = handleToken.WriteToken(token),
+      User = new UserRegisterDto
+      {
+        Username = user.Username,
+        Name = user.Name ?? "No Name",
+        Role = user.Role,
+        Password = user.Password,
+        Id = user.Id.ToString()
+      },
+      Message = "User logged successfully"
+    };
+  }
+
+  public async Task<User> Register(CreateUserDto createUserDto)
+  {
+    string encryptedPassword = BCrypt.Net.BCrypt.HashPassword(createUserDto.Password);
+    User user = new User()
+    {
+      Username = createUserDto.Username ?? "No Username",
+      Name = createUserDto.Name,
+      Role = createUserDto.Role,
+      Password = encryptedPassword
+    };
+
+    await _db.Users.AddAsync(user);
+    await _db.SaveChangesAsync();
+    return user;
+  }
+}
