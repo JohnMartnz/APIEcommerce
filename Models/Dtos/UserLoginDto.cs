d2cd5a0c9cc8b53cbb7bb9608ce69c5abb5106cc
John Mart <juanmart.nt@gmail.com>
2026-03-10 11:00:21 -0600

    Add User model and Dtos

---
+++ Models/Dtos/UserLoginDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIEcommerce.Models.Dtos;
+
+public class UserLoginDto
+{
+  [Required(ErrorMessage = "Username field is required")]
+  public string? Username { get; set; }
+
+  [Required(ErrorMessage = "Password field is required")]
+  public string? Password { get; set; }
+}
