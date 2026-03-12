7be23b31d08c08ac1fffaafcbe6233205ff398c3
John Mart <juanmart.nt@gmail.com>
2026-03-12 16:07:10 -0600

    Add authentication, CORS and authentication in Scalar client

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
+using APIEcommerce.Constants;
 using APIEcommerce.Data;
+using APIEcommerce.Helpers;
 using APIEcommerce.Repository;
 using APIEcommerce.Repository.IRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,9 +22,44 @@ builder.Services.AddAutoMapper(cfg =>
     cfg.AddMaps(typeof(Program).Assembly);
 });
 
+string? secretKey = builder.Configuration.GetValue<string>("ApiSettings:SecretKey");
+
+if (string.IsNullOrEmpty(secretKey))
+{
+    throw new InvalidOperationException("Secret Key is not valid");
+}
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(PolicyNames.AllowSpecificOrigin, builder =>
+    {
+        builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -38,6 +78,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(PolicyNames.AllowSpecificOrigin);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
