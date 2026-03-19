bb1ed65dbaba3bf419c4d64449e9aa4fd1ff4718
John Mart <juanmart.nt@gmail.com>
2026-03-18 23:14:57 -0600

    Remove comments

--- Helpers/MapperAdapter.cs
+++ Helpers/MapperAdapter.cs
@@ -2,18 +2,12 @@ using Mapster;
 
 namespace APIEcommerce.Helpers;
 
-/// <summary>
-/// Adaptador que proporciona una interfaz compatible con AutoMapper para usar con Mapster
-/// </summary>
 public interface IMapper
 {
   TDestination Map<TDestination>(object source);
   TDestination Map<TSource, TDestination>(TSource source);
 }
 
-/// <summary>
-/// Implementación de IMapper que usa Mapster internamente
-/// </summary>
 public class MapsterAdapter : IMapper
 {
   public TDestination Map<TDestination>(object source)
